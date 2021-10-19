4dce17b07b550ccb0b0b9fb759f28ae7ed77153b
saifulislammunna <saifulmunna28@gmail.com>
2021-10-20 00:10:23 +0600

    lowercase-uppercase-added

---
+++ lowercase-uppercase.c
@@ -0,0 +1,16 @@
+ #include <stdio.h>
+
+ int main()
+ {
+
+     char ch = 'w';
+
+     if(ch >= 'a' && ch <= 'z'){
+        printf("%c is lower case\n", ch);
+     }
+     if(ch >= 'A' && ch <= 'z'){
+        printf("%c is upper case\n", ch);
+     }
+
+     return 0;
+ }
